68a2c46742599c8ed8e5e3e6b7dfc135a6f91e7e
anhinz <anjahinz@ymail.com>
2022-03-09 20:41:18 +0100

    commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String tablicaStringow[] = new String[4];
+
+        tablicaStringow = new String[]{"Vita","Misia","Lucy","Sara"};
+        for (int i = 0; i < 4; i=i+2){
+            System.out.println(tablicaStringow[i]);
+        }
+
+
+    }
+}
